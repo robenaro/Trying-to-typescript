939a003f023fe4f07a83f331d8953b49d6f45c79
kka17 <infokka17@gmail.com>
2022-03-01 21:55:34 +0300

    Add todo array, interfaces and method - complit&remove

--- ts-react/src/components/TodoList.tsx
+++ ts-react/src/components/TodoList.tsx
@@ -1,19 +1,38 @@
 import React from "react";
+import { ITodo } from "../interfaces";
 
 type TodoListProps = {
-    todos: any[]
+    todos: ITodo[]
+    onToggle(id: number): void
+    onRemove: (id: number) => void
 }
 
-export const TodoList: React.FC<TodoListProps> = ({todos}) => {
+export const TodoList: React.FC<TodoListProps> = ({
+    todos, 
+    onRemove, 
+    onToggle
+}) => {
+    if (todos.length === 0) {
+        return <p className="center">Пока дел нет</p>
+    }
     return (
         <ul>
             {todos.map(todo => {
+                const classes = ['todo']
+                if (todo.completed) {
+                    classes.push('complited')
+                }
                 return (
-                    <li className="todo">
+                    <li className={classes.join(' ')} key={todo.id}>
                         <label htmlFor="">
-                            <input type="checkbox"/>
-                            <span></span>
-                            <i className="material-icons red-text">delete</i>
+                            <input type="checkbox" checked={todo.completed} onChange={onToggle.bind(null, todo.id)}/>
+                            <span>{todo.title}</span>
+                            <i 
+                                className="material-icons red-text" 
+                                onClick={() => onRemove(todo.id)}
+                            >
+                                delete
+                            </i>
                         </label>
                     </li>
                 )
