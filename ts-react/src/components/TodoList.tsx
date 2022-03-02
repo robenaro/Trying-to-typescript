f4c6d9b65c427a25e00875ab867cf8c72c7f1764
kka17 <infokka17@gmail.com>
2022-03-02 11:48:15 +0300

    Fixed a bug of checked input

--- ts-react/src/components/TodoList.tsx
+++ ts-react/src/components/TodoList.tsx
@@ -1,43 +1,53 @@
-import React from "react";
-import { ITodo } from "../interfaces";
+import React from 'react'
+import { ITodo } from '../interfaces'
 
 type TodoListProps = {
-    todos: ITodo[]
-    onToggle(id: number): void
-    onRemove: (id: number) => void
+  todos: ITodo[]
+  onToggle:(id: number) => void
+  onRemove: (id: number) => void
 }
 
 export const TodoList: React.FC<TodoListProps> = ({
-    todos, 
-    onRemove, 
-    onToggle
+  todos,
+  onRemove,
+  onToggle
 }) => {
-    if (todos.length === 0) {
-        return <p className="center">Пока дел нет</p>
-    }
-    return (
-        <ul>
-            {todos.map(todo => {
-                const classes = ['todo']
-                if (todo.completed) {
-                    classes.push('complited')
-                }
-                return (
-                    <li className={classes.join(' ')} key={todo.id}>
-                        <label htmlFor="">
-                            <input type="checkbox" checked={todo.completed} onChange={onToggle.bind(null, todo.id)}/>
-                            <span>{todo.title}</span>
-                            <i 
-                                className="material-icons red-text" 
-                                onClick={() => onRemove(todo.id)}
-                            >
-                                delete
-                            </i>
-                        </label>
-                    </li>
-                )
-            })}
-            
-        </ul>
-    )
+  if (todos.length === 0) {
+    return <p className="center">Пока дел нет!</p>
+  }
+
+  const removeHandler = (event: React.MouseEvent, id: number) => {
+    event.preventDefault()
+    onRemove(id)
+  }
+
+  return (
+    <ul>
+      {todos.map(todo => {
+        const classes = ['todo']
+        if (todo.completed) {
+          classes.push('completed')
+        }
+
+        return (
+          <li className={classes.join(' ')} key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={onToggle.bind(null, todo.id)}
+              />
+              <span>{todo.title}</span>
+              <i
+                className="material-icons red-text"
+                onClick={event => removeHandler(event, todo.id)}
+              >
+                delete
+              </i>
+            </label>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
